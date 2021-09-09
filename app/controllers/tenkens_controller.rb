977f327315e5bae577cd8650881920ee9a49d023
NaseBe <nazebesto@gmail.com>
2021-09-09 20:55:56 +0900

    点検編集機能を実装

--- app/controllers/tenkens_controller.rb
+++ app/controllers/tenkens_controller.rb
@@ -1,4 +1,5 @@
 class TenkensController < ApplicationController
+  before_action :set_tenken, only: [:show, :edit, :update]
   def index
     @tenken = Tenken.new
     @tenkens = Tenken.includes(:user).order('created_at DESC')
@@ -11,8 +12,27 @@ class TenkensController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @tenken.update(tenken_params)
+      redirect_to tenken_path(@tenken.id)
+    else
+      redirect_to action: :edit
+    end
+  end
+
   private
+
   def tenken_params
     params.require(:tenken).permit(:name).merge(user_id: current_user.id)
+  end
+
+  def set_tenken
+    @tenken = Tenken.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tenkens#index"
   resources :users, only: [:edit, :update]
-  resources :tenkens, only: [:index, :create]
+  resources :tenkens, only: [:index, :create, :show, :edit, :update]
 end
