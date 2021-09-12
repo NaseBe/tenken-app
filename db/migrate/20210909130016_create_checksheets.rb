c5ab1253e843049c838c608d517a24bb6d92cd44
NaseBe <nazebesto@gmail.com>
2021-09-12 11:59:03 +0900

    checksheetsテーブルを作成

--- db/migrate/20210909130016_create_checksheets.rb
+++ db/migrate/20210909130016_create_checksheets.rb
@@ -1,7 +1,25 @@
 class CreateChecksheets < ActiveRecord::Migration[6.0]
   def change
     create_table :checksheets do |t|
-
+      t.integer :classroom_id, null: false
+      t.string :projector
+      t.string :control_panel
+      t.string :mic
+      t.string :pc
+      t.string :bd_dvd_player
+      t.string :remotes
+      t.string :ohc
+      t.string :screen
+      t.string :ports
+      t.string :cables
+      t.string :expendables
+      t.text :details
+      t.string :staff, null: false
+      t.string :helper
+      t.date :date, null: false
+      t.string :status, null: false
+      t.references :tenken, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
