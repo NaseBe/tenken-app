314af2d28b51e337fc3bf99dca3bb55b73bad9f6
NaseBe <nazebesto@gmail.com>
2021-09-14 19:03:16 +0900

    チェックシート編集画面を実装

--- app/controllers/checksheets_controller.rb
+++ app/controllers/checksheets_controller.rb
@@ -1,8 +1,9 @@
 class ChecksheetsController < ApplicationController
+  before_action :set_users
   before_action :set_tenken
+  before_action :set_checksheet, only: [:show, :edit, :update]
 
-  def new
-    @users = User.all
+  def new  
   end
 
   def create
@@ -13,16 +14,38 @@ class ChecksheetsController < ApplicationController
   end
 
   def show
-    @checksheet = @tenken.checksheets.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @checksheet.update(update_params)
+      redirect_to tenken_path(@tenken.id)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_users
+    @users = User.all
+  end
 
   def set_tenken
     @tenken = Tenken.find(params[:tenken_id])
   end
 
+  def set_checksheet
+    @checksheet = @tenken.checksheets.find(params[:id])
+  end
+
   def checksheet_params
     params.permit(:classroom_id, :projector, :control_panel, :mic, :pc, :bd_dvd_player, :remotes, :ohc, :screen, :ports, :cables, :expendables, :details, :staff, :helper, :date, :status).merge(tenken_id: params[:tenken_id], user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:checksheet).permit(:classroom_id, :projector, :control_panel, :mic, :pc, :bd_dvd_player, :remotes, :ohc, :screen, :ports, :cables, :expendables, :details, :staff, :helper, :date, :status).merge(tenken_id: params[:tenken_id], user_id: current_user.id)
   end
 end
