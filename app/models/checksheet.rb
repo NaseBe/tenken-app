600c3fc319cce91732275d35e7c6768530381542
NaseBe <nazebesto@gmail.com>
2021-09-18 17:18:52 +0900

    チェックシート画像投稿処理を実装

--- app/models/checksheet.rb
+++ app/models/checksheet.rb
@@ -4,6 +4,7 @@ class Checksheet < ApplicationRecord
   belongs_to :tenken
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_one_attached :image
 
   with_options presence: true do
     validates :classroom_id, numericality: { other_than: 0, message: "can't be blank" }
